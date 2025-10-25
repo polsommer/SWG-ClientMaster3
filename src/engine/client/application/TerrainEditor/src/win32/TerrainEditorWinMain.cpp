6e96eb167deca31e26cac0382f6bf7bcadcb8f72
polsommer <patricker@live.dk>
2025-10-25 18:43:15 +0200

    Fix TerrainEditor entry points for Unicode builds

--- src/engine/client/application/TerrainEditor/src/win32/TerrainEditorWinMain.cpp
+++ src/engine/client/application/TerrainEditor/src/win32/TerrainEditorWinMain.cpp
@@ -1,11 +1,42 @@
 #include "FirstTerrainEditor.h"
 
-#include <afxwin.h>
 #include <tchar.h>
+
+#include <cstddef>
+#include <vector>
 
 extern int AFXAPI AfxWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow);
 
-int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+namespace
 {
-    return AfxWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+    int invokeAfxWinMain(HINSTANCE const hInstance, HINSTANCE const hPrevInstance, LPTSTR const lpCmdLine, int const nCmdShow)
+    {
+        return AfxWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+    }
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    return invokeAfxWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+}
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    if (!lpCmdLine)
+    {
+        return invokeAfxWinMain(hInstance, hPrevInstance, GetCommandLineA(), nCmdShow);
+    }
+
+    int const requiredCharacterCount = WideCharToMultiByte(CP_ACP, 0, lpCmdLine, -1, nullptr, 0, nullptr, nullptr);
+
+    if (requiredCharacterCount <= 0)
+    {
+        return invokeAfxWinMain(hInstance, hPrevInstance, GetCommandLineA(), nCmdShow);
+    }
+
+    std::vector<char> ansiCommandLine(static_cast<std::size_t>(requiredCharacterCount));
+
+    WideCharToMultiByte(CP_ACP, 0, lpCmdLine, -1, ansiCommandLine.data(), requiredCharacterCount, nullptr, nullptr);
+
+    return invokeAfxWinMain(hInstance, hPrevInstance, ansiCommandLine.data(), nCmdShow);
 }
