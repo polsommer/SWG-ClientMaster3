e075e15d474017ff93fef7241fe86a598a7c7d33
polsommer <patricker@live.dk>
2025-10-25 18:27:40 +0200

    Fix TerrainEditor entry point and output naming

---
+++ src/engine/client/application/TerrainEditor/src/win32/TerrainEditorWinMain.cpp
@@ -0,0 +1,9 @@
+#include "FirstTerrainEditor.h"
+
+#include <afxwin.h>
+#include <tchar.h>
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+    return AfxWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+}
